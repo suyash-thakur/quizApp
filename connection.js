80b4d47ef4944ca9fc525476c0c38475705b9f16
Hardik <hardikkhandelwal500@gmail.com>
2020-05-17 19:25:11 +0530

    [add] table config

--- connection.js
+++ connection.js
@@ -5,7 +5,8 @@ const connection = mysql.createConnection({
     host     : process.env.RDS_HOSTNAME,
     user     : process.env.RDS_USERNAME,
     password : process.env.RDS_PASSWORD,
-    port     : process.env.RDS_PORT
+    port     : process.env.RDS_PORT,
+    database : process.env.RDS_DATABASE
 });
 
 connection.connect((err) => {
@@ -13,6 +14,10 @@ connection.connect((err) => {
       console.error('Database connection failed: ' + err.stack);
       return;
     }
-  
-    console.log('Connected to database.');
+    console.log('Connected to mysql.');
+    var sql = "select * from users;";
+    connection.query(sql, function (err, result) {
+    if (err) throw err;
+      console.log(result);
+    });
 });
