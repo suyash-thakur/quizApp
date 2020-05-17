2f123185eb22300cf33f5fb48ab048201fee7e9b
suyash-thakur <47927368+suyash-thakur@users.noreply.github.com>
2020-05-17 23:08:49 +0530

    Quiz dropdown fixxed

--- connection.js
+++ connection.js
@@ -5,7 +5,12 @@ const connection = mysql.createConnection({
     host     : process.env.RDS_HOSTNAME,
     user     : process.env.RDS_USERNAME,
     password : process.env.RDS_PASSWORD,
-    port     : process.env.RDS_PORT
+    port     : process.env.RDS_PORT,
+    ssl  : {
+      // DO NOT DO THIS
+      // set up your ca correctly to trust the connection
+      rejectUnauthorized: false
+    }
 });
 
 connection.connect((err) => {
