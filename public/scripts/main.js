0cfe03f1fd633895176387abf032d9be72144041
suyash-thakur <47927368+suyash-thakur@users.noreply.github.com>
2020-05-19 21:59:06 +0530

    Question Details Displayed, Login Hidden

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,3 +11,49 @@ function onInput() {
   location.href += `question?type=${this.questType}&operation=${this.operation}`;
 
 }
+var isLoggedin = false;
+function getCookie(c_name) {
+var i, x, y, ARRcookies = document.cookie.split(";");
+for (i = 0; i < ARRcookies.length; i++) {
+x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+x = x.replace(/^\s+|\s+$/g, "");
+if (x == c_name) {
+    return unescape(y);
+}
+}
+}
+function delete_cookie(name) {
+document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+document.getElementById('name').style.display = 'none';
+document.getElementById('signout').style.display = 'none';
+document.getElementById('login').style.display = 'block';
+document.getElementById('login-button').style.display = 'block';
+
+
+
+}
+function quizUrl() {
+if(isLoggedin == true) {
+window.location.href = "/quiz";
+} else {
+window.alert("Please log-in to continue");
+}
+}
+var x = getCookie("userData");
+console.log(x);
+if(x !== undefined) {
+isLoggedin = true;
+document.getElementById('name').style.display = 'block';
+document.getElementById('name').innerHTML = x;
+document.getElementById('signout').style.display = 'block';
+document.getElementById('login').style.display = 'none';
+document.getElementById('login-button').style.display = 'none';
+
+
+
+
+}
+
+console.log(x);
+console.log(isLoggedin);
