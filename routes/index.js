51a35687841507b2b0c7b4b3077af4b9aa26a478
Hardik <hardikkhandelwal500@gmail.com>
2020-06-01 02:16:45 +0530

    [add] flash card logic

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require("express"),
-      router = express.Router();
+  router = express.Router();
 const generateQuestion = require("../helper/questions");
 const randomNumber = require("../helper/randomNumber");
 const shuffle = require("../helper/arrayShuffle");
 const cookieParser = require("cookie-parser");
-const fs = require('fs');
+const fs = require("fs");
 const connection = require("../connection");
 const problem = fs.readFileSync(__dirname + "/wordproblem.json");
+const flash = fs.readFileSync(__dirname + "/flashCard.json");
 
 const quizFunctionCall = {
   one: 7,
@@ -33,7 +34,9 @@ let questionArr = [];
 
 let useVar = {};
 
-questionsGenerate = (typeOfQuestion) => {
+let userID = 440 || randomNumber(1, 10000);
+
+const questionsGenerate = (typeOfQuestion) => {
   numOfQuestion = quizFunctionCall[typeOfQuestion];
   questionArr = [];
 
@@ -48,12 +51,11 @@ questionsGenerate = (typeOfQuestion) => {
     let choices = [answer];
     let i = 0;
 
-    while(i < 3) {
-      let num = randomNumber(answer - 5, answer + 5) ;
-      if(!choices.includes(num - 0) ){
-        console.log(choices.includes(num));
+    while (i < 3) {
+      let num = randomNumber(answer - 5, answer + 5);
+      if (!choices.includes(num - 0)) {
         choices.push(num - 0);
-        i++
+        i++;
       }
     }
 
@@ -64,29 +66,36 @@ questionsGenerate = (typeOfQuestion) => {
 
   return questionArr;
 };
-var userData = null;
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index");
 });
 
-router.post('/', function(req, res) {
-  const {email, password} = req.body;
+router.post("/", function (req, res) {
+  const { email, password } = req.body;
 
   if (email && password) {
-		connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
-			if (results.length > 0) {
-        const name = results[0].name;
-        res.cookie('userData', name);
-        res.cookie('profileData', {name: results[0].name, email: results[0].email});
-        res.redirect('/');
-			} else {
-        res.render("index", {error: 'Incorrect email and/or Password!'});
-			}			
-		});
-	} else {
-    res.render("index", {error: 'Please enter email and Password!'});
-	}
+    connection.query(
+      "SELECT * FROM users WHERE email = ? AND password = ?",
+      [email, password],
+      function (error, results, fields) {
+        if (results.length > 0) {
+          const name = results[0].name;
+          res.cookie("userData", name);
+          res.cookie("profileData", {
+            name: results[0].name,
+            email: results[0].email,
+          });
+          res.redirect("/");
+        } else {
+          res.render("index", { error: "Incorrect email and/or Password!" });
+        }
+      }
+    );
+  } else {
+    res.render("index", { error: "Please enter email and Password!" });
+  }
 });
 
 router.get("/quiz", function (req, res, next) {
@@ -103,53 +112,112 @@ router.get("/DemoQuiz", function (req, res, next) {
 });
 
 router.get("/profile", function (req, res, next) {
-  res.render("profile", {profileData: req.cookies.profileData});
+  res.render("profile", { profileData: req.cookies.profileData });
 });
+
 router.get("/wordProblem", function (req, res, next) {
-let problems = JSON.parse(problem);
-arrayOfQuestions = [];
-arrayOfChoices = [];
-arrayofAnswers = [];
+  let problems = JSON.parse(problem);
+  arrayOfQuestions = [];
+  arrayOfChoices = [];
 
-problems.forEach((problem) => {
-  arrayOfQuestions.push(problem.question);
-  arrayOfChoices.push(shuffle(problem.options));
-  arrayofAnswers.push(shuffle(problem.answer));
+  problems.forEach((problem) => {
+    arrayOfQuestions.push(problem.question);
+    arrayOfChoices.push(shuffle(problem.options));
+  });
 
-});
-
-console.log(arrayOfChoices);
-console.log(arrayOfQuestions);
+  useVar[userID] = {"problem" :problems};
 
   res.render("wordProblem", {
     questions: arrayOfQuestions,
-    choices: arrayOfChoices
+    choices: arrayOfChoices,
   });
 });
 
-router.post("/profile", function(req, res) {
-  const {username, email, password} = req.body;
+router.post("/wordProblem", function (req, res, next) {
+  let flashJSON = useVar[userID]["problem"];
   
-  connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], function(error, results) {
-    if (results.length > 0) {
-      const name = results[0].name;
-      res.cookie('userData', name);
-      res.cookie('profileData', {name: results[0].name, email: results[0].email});
-      connection.query('UPDATE users SET name= ? WHERE email = ? AND password = ?',[username, email, password] , function(error, results) {
-         res.status(200).redirect("/");
-      });
-    } else {
-      res.render("profile", {profileData: req.cookies.profileData, error: 'Incorrect Password!'});
+  const arr = req.body;
+  let points = 0;
+  
+  flashJSON.forEach((e, i) => {
+    if(e.answer == arr[i]){
+      points += 1;
     }
   })
+
+  res.send({points})
 })
+
+router.get("/flashCard", function (req, res, next) {
+  let flashJSON = JSON.parse(flash);
+  let flashChoices = [];
+  let flashQuestion = [];
+
+  flashJSON.forEach((e) => {
+    flashQuestion.push(e.question);
+    flashChoices.push(e.options);
+  })
+  
+  useVar[userID] = {"flash" :flashJSON};
+  res.render("flashCard", {
+    flashQuestion,
+    flashChoices,
+  });
+
+});
+
+router.post("/flashCard", function (req, res, next) {
+  let flashJSON = useVar[userID]["flash"];
+  
+  const arr = req.body;
+  let points = 0;
+  
+  flashJSON.forEach((e, i) => {
+    if(e.answer == arr[i]){
+      points += 1;
+    }
+  })
+
+  res.send({points})
+})
+
+router.post("/profile", function (req, res) {
+  const { username, email, password } = req.body;
+
+  connection.query(
+    "SELECT * FROM users WHERE email = ? AND password = ?",
+    [email, password],
+    function (error, results) {
+      if (results.length > 0) {
+        const name = results[0].name;
+        res.cookie("userData", name);
+        res.cookie("profileData", {
+          name: results[0].name,
+          email: results[0].email,
+        });
+        connection.query(
+          "UPDATE users SET name= ? WHERE email = ? AND password = ?",
+          [username, email, password],
+          function (error, results) {
+            res.status(200).redirect("/");
+          }
+        );
+      } else {
+        res.render("profile", {
+          profileData: req.cookies.profileData,
+          error: "Incorrect Password!",
+        });
+      }
+    }
+  );
+});
 
 router.get("/quizquestion", function (req, res, next) {
   typeOfQuestion = req.query.type;
   operation = req.query.operation;
 
   questionArr = questionsGenerate(typeOfQuestion, operation);
-
+  
   arrayOfQuestions = [];
   arrayOfChoices = [];
 
@@ -158,40 +226,30 @@ router.get("/quizquestion", function (req, res, next) {
     arrayOfChoices.push(shuffle(quiz.choices));
   });
 
-  userID = randomNumber(1, 1000);
-
-  useVar[userID] = questionArr;
+  useVar[userID] = {"quiz" :questionArr};
   res.cookie("userID", userID); // options is optional
-  console.log(arrayOfQuestions);
-  console.log(arrayOfChoices);
 
-
-   return res.render("question", {
+  res.render("question", {
     quizQuestion: arrayOfQuestions,
     quizChoices: arrayOfChoices,
-    operation: operation
+    operation: operation,
   });
-
 });
 
-router.get("/wordProblem", function (req, res, next) {
-
-res.render("wordProblem");
-
-});
-router.post("/api", function (req, res, next) {
-  result = useVar[req.cookies.userID][req.body.index].answer == req.body.answer;
+router.post("/quizquestion", function (req, res, next) {
+  result = useVar[req.cookies.userID]["quiz"][req.body.index].answer == req.body.answer;
   res.json({
     result: result,
-    answer: useVar[req.cookies.userID][req.body.index].answer,
+    answer: useVar[req.cookies.userID]["quiz"][req.body.index].answer,
   });
 });
+
 router.post("/check", function (req, res, next) {
   let isCorrect;
-  if( req.body.result === arrayofAnswers[req.body.id]) {
-    isCorrect = true
+  if (req.body.result === arrayofAnswers[req.body.id]) {
+    isCorrect = true;
   } else {
-    isCorrect = false
+    isCorrect = false;
   }
   res.json({
     check: isCorrect,
@@ -199,10 +257,10 @@ router.post("/check", function (req, res, next) {
   });
 });
 
-router.post("/clear", function (req,res,next) {
-  res.clearCookie('profileData');
-  res.clearCookie('userData');
+router.post("/clear", function (req, res, next) {
+  res.clearCookie("profileData");
+  res.clearCookie("userData");
   res.redirect("/");
-})
+});
 
 module.exports = router;
